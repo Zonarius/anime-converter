6748094e1aea19440cb55688105bffd32469622b
zonarius <zonarius90@gmail.com>
2017-02-11 22:49:36 +0100

    fix delete in frontend

--- src/server/static/js/status.js
+++ src/server/static/js/status.js
@@ -1,15 +1,18 @@
 Vue.component('queued-anime', {
   props: ['anime'],
-  computed: {
-    deleteUrl: function () {
-      if (this.anime) {
-        return "/delete/" + this.anime.id
-      } else {
-        return "#";
-      }
+  methods: {
+    deleteAnime: function(anime) {
+      fetch("/delete/" + anime.id, {
+        credentials: "same-origin"
+      }).then(it => it.json())
+        .then(it => {
+          if (it.message === "ok") {
+            vm.status.queue = it.queue
+          }
+        })
     }
   },
-  template: '<li class="list-group-item"><span class="badge"><a :href="deleteUrl"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a></span>{{anime.filename}}</li>'
+  template: '<li class="list-group-item"><span class="badge"><a href="#" @click="deleteAnime(anime)"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a></span>{{anime.filename}}</li>'
 })
 
 
@@ -28,6 +31,8 @@ var vm = new Vue({
     }
   }
 });
+
+
 
 function fetchStatus() {
   fetch("/status", {
